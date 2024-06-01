9a9c831c98caa1d79f9dcb04c02ab5f711a01f09
Kaushik Mandal <kaushikmandal27@gmail.com>
2024-06-01 23:45:54 +0530

    updated operations to use json-server

--- src/App.js
+++ src/App.js
@@ -1,61 +1,100 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header  from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
-  const [showAddTask, setShowAddtask] = useState(false)
-  const [tasks, setTasks ] = useState([
-    {
-        id: 1,
-        text: 'Doctor appointment',
-        day: 'June 5 at 7:00pm',
-        reminder: false
-    },
-    {
-        id: 2,
-        text: 'Playtime with Oscar',
-        day: 'Feb 6 at 2:00pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Project Interview',
-        day: 'Feb 7 at 2:00pm',
-        reminder: false
+  const [showAddTask, setShowAddtask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
     }
-]) 
+    getTasks()
+  }, [])
 
-// Add task
-const addTask = (task) => {
-  console.log('add ', task)
-  const id = Math.floor(Math.random() * 10000 + 1)
-  const newTask = { ...task, id }
-  setTasks([...tasks, newTask])
-}
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    console.log("fetched tasks", data);
+    return data
+  }
 
-// Delete task
-const deleteTask = (id) => {
-  console.log('delete ', id)
-  setTasks(tasks.filter((task) => task.id != id))
-}
+    // Fetch task
+    const fetchTask = async (id) => {
+      console.log("fetch task", id);
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      return data
+    }
 
-// Toggle reminder
-const toggleReminder = (id) => {
-  console.log('toggle ', id)
-  setTasks(
-    tasks.map((task) =>
-      task.id === id ? { ...task, reminder: !task.reminder } : task
-    )
-  );
-}
+  // Add task
+  const addTask = async (task) => {
+    console.log("add task", task);
+    const res = await fetch(`http://localhost:5000/tasks`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task),
+    })
 
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    
+    // console.log("add ", task);
+    // const id = Math.floor(Math.random() * 10000 + 1);
+    // const newTask = { ...task, id };
+    // setTasks([...tasks, newTask]);
+  };
+
+  // Delete task
+  const deleteTask = async (id) => {
+    console.log("delete task", id);
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id != id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = async (id) => {
+    console.log("toggle task", id);
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask),
+    }) 
+
+    const data = await res.json()
+    
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !data.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddtask(!showAddTask)} showAdd = {showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      <Header
+        onAdd={() => setShowAddtask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
